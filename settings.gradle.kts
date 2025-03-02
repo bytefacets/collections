1b64a7d7f1ceb812a97216722b703d1a60f4bffe
paulfeuer <code@bytefacets.com>
2025-03-02 07:08:33 -0500

    use something that can access across repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,7 @@ pluginManagement {
                 url = uri("https://maven.pkg.github.com/bytefacets/type-template-processor")
                 credentials {
                     username = System.getenv("USERNAME") ?: providers.gradleProperty("gpr.user").orElse("no-username").get()
-                    password = "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"
-//                    password = System.getenv("TOKEN") ?: providers.gradleProperty("gpr.key").orElse("no-token").get()
-                    System.out.println("GitHubPackages.u=" + username)
-                    System.out.println("GitHubPackages.p=" + password)
+                    password = System.getenv("TOKEN") ?: providers.gradleProperty("gpr.key").orElse("no-token").get()
                 }
             }
         }
